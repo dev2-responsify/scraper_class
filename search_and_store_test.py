163d630889d447b7c298980472c09f04cb7963a2
dev1-responsify <dev1@responsify.com>
2019-06-28 12:49:53 -0400

    testing selenium

--- search_and_store_test.py
+++ search_and_store_test.py
@@ -1,45 +1,31 @@
 # from selenium import webdriver
 # from selenium.webdriver.common.keys import Keys
 # import time
-import urllib
-from bs4 import BeautifulSoup
-import requests
-import webbrowser
-
-text = 'hello world'
-text = urllib.parse.quote_plus(text)
-
-url = 'https://google.com/search?q=' + text
-
-response = requests.get(url)
-
-#with open('output.html', 'wb') as f:
-#    f.write(response.content)
-#webbrowser.open('output.html')
-
-soup = BeautifulSoup(response.text, 'lxml')
-for g in soup.find_all(class_='g'):
-    print(g.text)
-    print('-----')
 
 
-# driver = webdriver.Chrome("/Users/responsify/Desktop/selenium_project/drivers/chromedriver")
-
-# driver.set_page_load_timeout(10)
-# driver.get("http://google.com")
-# driver.refresh()
 
 
-# def repeat(n):
-#     for i in range(n):
-#         driver = webdriver.Chrome("/Users/responsify/Desktop/dev1/seleniumProj2/drivers/chromedriver")
-#         driver.set_page_load_timeout(10)
-#         driver.get("http://google.com")
-#         #driver.refresh()
-#         element_search = driver.find_element_by_name("q")
-#         element_search.send_keys("Warcraft")
-#         element_search.send_keys(Keys.RETURN)
+# import requests
+# from bs4 import BeautifulSoup
+
+# r = requests.get('https://www.google.com/search?q=site:wikipedia.com+Black+hole&gbv=1&sei=YwHNVpHLOYiWmQHk3K24Cw')
+# soup = BeautifulSoup(r.text, "html.parser")
+# store = soup.find_all('span')
+
+# print(store)
+
+
+
+def repeat(n):
+    for i in range(n):
+        driver = webdriver.Chrome("/Users/responsify/Desktop/dev1/seleniumProj2/drivers/chromedriver")
+        driver.set_page_load_timeout(10)
+        driver.get("https://www.linkedin.com/")
+        #driver.refresh()
+        element_search = driver.find_element_by_name("q")
+        element_search.send_keys("Warcraft")
+        element_search.send_keys(Keys.RETURN)
   
 
 
-# repeat(1)
+repeat(1)
