0f98e8f8a601d0bd01cde379dceea3587857c7ed
dev1-responsify <dev1@responsify.com>
2019-06-28 11:20:51 -0400

    Searching through google

--- search_and_store_test.py
+++ search_and_store_test.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
 
 
@@ -14,7 +16,10 @@ def repeat(n):
         driver = webdriver.Chrome("/Users/responsify/Desktop/dev1/seleniumProj2/drivers/chromedriver")
         driver.set_page_load_timeout(10)
         driver.get("http://google.com")
-        driver.refresh()
+        #driver.refresh()
+        element_search = driver.find_element_by_name("q")
+        element_search.send_keys("Warcraft")
+        element_search.send_keys(Keys.RETURN)
 
 
 repeat(1)
